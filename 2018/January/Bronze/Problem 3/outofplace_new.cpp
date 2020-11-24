a1df118cd042fd9038233d0ae747ce673ef31c25
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2020-11-24 13:22:21 -0800

    a

---
+++ 2018/January/Bronze/Problem 3/outofplace_new.cpp
@@ -0,0 +1,57 @@
+//undetermined
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+ifstream fin ("outofplace.in");
+ofstream fout ("outofplace.out");
+
+int main()
+{
+    int N, temp = 0, b_index = -1, b_val = -1, swaps, supposed_index, temp1 = 0, temp2 = 0;
+	fin >> N;
+	int A[N];
+	for (int i = 0; i < N; i++){
+		fin >> A[i];
+	}
+	for (int i = 0; i < N; i++){
+		if ((temp1 <= A[i]) & (temp2 > A[i] or temp2 < temp1)){
+			b_index = i-1;
+			b_val = A[i-1];
+			break;
+		}
+		temp1 = temp2;
+		temp2 = A[i];
+	}
+	if (b_index == -1){
+		b_index = N-1;
+		b_val = A[N];
+	}
+	temp = 0;
+	for (int i = 0; i < N; i++){
+		if (b_val >= temp & b_val <= A[i] & i != b_index & i-1 != b_index){
+			if (b_index > i) supposed_index = i-1;
+			if (b_index < i) supposed_index = i;
+			if ((A[i] == b_val) & b_index > i) supposed_index = i;
+			if ((temp == b_val) & b_index > i) supposed_index = i-1;
+			if ((A[i] == b_val) & b_index < i) supposed_index = i;
+			if ((temp == b_val) & b_index < i) supposed_index = i-1;
+			break;
+		}
+		temp = A[i];
+	}
+	cout << supposed_index << "\n" << b_index << "\n";
+	vector<int> used_nums;
+	for (int i = 0; i < N; i++){
+		if (std::find(used_nums.begin(), used_nums.end(), A[i]) == used_nums.end() & ((i > supposed_index & i < b_index) or (i < supposed_index & i > b_index))){
+			used_nums.push_back(A[i]);
+		}
+	}
+	for (int i = 0; i < used_nums.size(); i++){
+		cout << used_nums[i] << "\n";
+	}
+	
+	fout << used_nums.size();
+}
