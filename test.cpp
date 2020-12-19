604ed76bf15c941364f38351bc8952d9b6db9f8d
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2020-12-19 12:11:44 -0800

    creat art.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    int a[1], b[1];
-    b[0]=1;
-    a[0]=b[0];
-    b[0]=2;
-    std::cout << a[0] << b[0];
+    int i;
+    cin >> i;
+    cout << i << "\n";
 }
