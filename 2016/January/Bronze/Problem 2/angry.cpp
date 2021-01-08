ec09ce602216bdf358d4820a1ef685645e17ded2
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2021-01-07 19:39:02 -0800

    create angry.cpp

---
+++ 2016/January/Bronze/Problem 2/angry.cpp
@@ -0,0 +1,58 @@
+//142 minutes
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+int N, ans = 0;
+int arr[101];
+
+int main() {
+    ifstream fin ("angry.in");
+    ofstream fout ("angry.out");
+    fin >> N;
+    for (int i = 0; i < N; i++) fin >> arr[i];
+    sort(arr, arr + N);
+    
+    for (int i = 0; i < N; i++){
+        int num = 1;
+        int center = i, radius = 1;
+        while (true){
+            int rMost = center;
+            int j = center + 1;
+            while (arr[j] <= arr[center] + radius && j < N){
+                num++;
+                rMost = j;
+                j++;
+            }
+            
+            if (rMost == center) break;
+            else{
+                center = rMost;
+                radius++;
+            }
+        }
+        
+        center = i, radius = 1;
+        while (true){
+            int lMost = center;
+            int j = center - 1;
+            while (arr[j] >= arr[center] - radius && j >= 0){
+                num++;
+                lMost = j;
+                j--;
+            }
+            
+            if (lMost == center) break;
+            else{
+                center = lMost;
+                radius++;
+            }
+        }
+        
+        ans = max(ans, num);
+    }
+    
+    fout << ans << "\n";
+    return 0;
+}
